2aa36723f794c0c7699a11125f25f8bdcdcf221f
Djcharles26 <jlaraanaya@gmail.com>
2022-10-25 10:03:52 -0500

    Updated flutter payments library

--- lib/flutter_payments.dart
+++ lib/flutter_payments.dart
@@ -1,2 +1,15 @@
 library flutter_payments;
 
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+export 'package:flutter_custom_payments/generated/l10n.dart';
+
+export './providers/stripe_payments.dart';
+export './screens/card_input.dart';
+export './screens/payment_methods.dart';
+
+Future<void> configureStripe (String publishableKey) async {
+  Stripe.publishableKey = publishableKey;
+
+  await Stripe.instance.applySettings();
+}
